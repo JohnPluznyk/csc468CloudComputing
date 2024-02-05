fad54f124105e6b8e4a0544dd41039ca369bd58c
JohnPluznyk <115428211+JohnPluznyk@users.noreply.github.com>
2024-02-04 21:39:04 -0500

    Update profile.py
    Updated version of Ubuntu from 18 to 22

--- profile.py
+++ profile.py
@@ -5,7 +5,7 @@ import geni.rspec.pg as rspec
 request = portal.context.makeRequestRSpec()
 # Create a XenVM
 node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
